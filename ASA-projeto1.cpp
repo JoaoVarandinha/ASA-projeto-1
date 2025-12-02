97e656372a5349f4f8e4a73ff93749feb8fdafe8
bruna_almeida <bruna.almeida@tecnico.ulisboa.pt>
2025-12-02 16:17:44 +0000

    Co-authored-by: Joao-Varanda <Joao-Varanda@users.noreply.github.com>

--- ASA-projeto1.cpp
+++ ASA-projeto1.cpp
@@ -17,7 +17,7 @@ int affinity[5][5] {
 
 //Convert class to index
 int biochemicalClass(char c) {
-    switch(c) {
+    switch (c) {
         case 'P': return 0;
         case 'N': return 1;
         case 'A': return 2;
@@ -28,7 +28,7 @@ int biochemicalClass(char c) {
 }
 
 //Calculates the energy released when removing the amino acid at position i
-long long calculateEnergy(int posLeft, int posMiddle, int posRight, const vector<int>& stabilityPotentials, 
+unsigned long long calculateEnergy(int posLeft, int posMiddle, int posRight, const vector<int>& stabilityPotentials, 
                                                                     const vector<int>& biochemicalClasses) {
     
     //Calculate the potencial(P) of each one
@@ -42,40 +42,41 @@ long long calculateEnergy(int posLeft, int posMiddle, int posRight, const vector
     int BC_right = (posRight == biochemicalClasses.size()) ? biochemicalClass('T') : biochemicalClasses[posRight];
 
     //Calculate energy
-    long long E1 = P_left*affinity[BC_left][BC_middle]*P_middle;
-    long long E2 = P_middle*affinity[BC_middle][BC_right]*P_right;
+    unsigned long long E1 = P_left*affinity[BC_left][BC_middle]*P_middle;
+    unsigned long long E2 = P_middle*affinity[BC_middle][BC_right]*P_right;
 
     return E1 + E2;
 }
 
 
 
-long long calculateTotalEnergy(int n, int l, int r, int k, const vector<vector<long long>>& maxEnergy,  
+unsigned long long calculateTotalEnergy(int n, int l, int r, int k, const vector<vector<unsigned long long>>& maxEnergy,  
                             const vector<int> stabilityPotentials, const vector<int> biochemicalClasses) {
-    long long leftEnergy = (k > l) ? maxEnergy[l][k-1] : 0;
-    long long rightEnergy = (k < l) ? maxEnergy[k+1][r] : 0;
+    unsigned long long leftEnergy = (k > l) ? maxEnergy[l][k-1] : 0;
+    unsigned long long rightEnergy = (k < l) ? maxEnergy[k+1][r] : 0;
 
     //Calculate energy when removing k last
     int posMiddle = k;
     int posLeft = (l == 0) ? -1 : l - 1;
     int posRight = (r == n - 1) ? n : r + 1;
-    long long kEnergy = calculateEnergy(posLeft, posMiddle, posRight, stabilityPotentials, biochemicalClasses);
+    unsigned long long kEnergy = calculateEnergy(posLeft, posMiddle, posRight, stabilityPotentials, biochemicalClasses);
 
     return leftEnergy + kEnergy + rightEnergy;
 }
 
 
 
-void fillMaxEnergyTable(int n, vector<vector<long long>>& maxEnergy, vector<vector<int>>& lastRemoved, 
+void fillMaxEnergyTable(int n, vector<vector<unsigned long long>>& maxEnergy, vector<vector<int>>& lastRemoved, 
                             const vector<int> stabilityPotentials, const vector<int> biochemicalClasses){
     //Base cases (intervals of size 1)
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int posMiddle = i;
-        int posLeft = (i == 0) ? -1 : i - 1;
-        int posRight = (i == n - 1) ? n : i + 1;
+        int posLeft = i - 1;
+        int posRight = i + 1;
         maxEnergy[i][i] = calculateEnergy(posLeft, posMiddle, posRight, stabilityPotentials, biochemicalClasses);
         lastRemoved[i][i] = posMiddle;
     }
+
 
     //Fill maxEnergy table for intervals of increasing size (iterative)
     for(int intervalSize = 2; intervalSize <= n; intervalSize++) {
@@ -84,7 +85,7 @@ void fillMaxEnergyTable(int n, vector<vector<long long>>& maxEnergy, vector<vect
 
             //Initialize with first value
             int k = l;
-            long long bestEnergy = calculateTotalEnergy(n, l, r, k, maxEnergy, stabilityPotentials, biochemicalClasses);
+            unsigned long long bestEnergy = calculateTotalEnergy(n, l, r, k, maxEnergy, stabilityPotentials, biochemicalClasses);
             int bestK = k;
         }
         //FIX ME -- for cm o k
@@ -115,9 +116,10 @@ int main() {
     }
 
 
+    
 
     //maxEnergy[l][r] = maximum energy when removing all amino acids in interval [l,r]
-    vector<vector<long long>> maxEnergy(n, vector<long long>(n, 0));
+    vector<vector<unsigned long long>> maxEnergy(n, vector<unsigned long long>(n, 0));
     //lastRemoved[l][r] = last amino acid removed in interval [l,r]
     vector<vector<int>> lastRemoved(n, vector<int>(n, -1));
 
